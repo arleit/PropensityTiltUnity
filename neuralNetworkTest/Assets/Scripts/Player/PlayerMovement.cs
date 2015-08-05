6c2f22deeb36b1349b9d5080ef06c5c403b26b78
Bryan Quinlan <quinlanb@wit.edu>
2015-08-05 16:56:32 -0400

    Map complete, several classes built but far from complete

--- neuralNetworkTest/Assets/Scripts/Player/PlayerMovement.cs
+++ neuralNetworkTest/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,12 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    void Start(){
+
+	}
+
+	void Update(){
+	
+		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime * 3,0f,Input.GetAxis("Vertical")* Time.deltaTime * 3);
+	}
 }
---
+++ neuralNetworkTest/Assets/aiController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/* aiController is represented by your base panel, it controls spawning
+ * aiController is extended by PlayerController
+ * it sets up listeners, spawns Units and SquadLeaders, and is itself a cp
+ * base health is tracked by the aiController as well as the enemy controller
+ * 
+ * 
+ */
+
+public class aiController : MonoBehaviour {
+
+	aiController opponent;
+	int cp1, cp2, cp3, cpBase, score, health;
+	//List<SquadLeader> SquadList;
+	//ArrayList<Unit> FieldUnitList, BaseUnitList;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
